8e713171139573e0440e28fff774fc333b2522ac
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-08-07 11:44:15 -0500

    cleaned up some age filtering modal code and added ability to show different alert messages

--- app/controllers/filterActivationModal.js
+++ app/controllers/filterActivationModal.js
@@ -28,6 +28,8 @@ var viewServicePath = setPathForLibDirectory('customCalls/viewService');
 var viewService = new viewServicePath();
 var loadingSpinner = setPathForLibDirectory('loadingSpinner/loadingSpinner');
 var spinner = new loadingSpinner();
+var filterService = setPathForLibDirectory('filterService/filterService');
+var filter = new filterService();
 
 function addSpinner() {
 	spinner.addTo($.win);
@@ -76,7 +78,7 @@ function createFilterView(filter, allChecked) {
 		right : '15%',
 		titleOn : " ",
 		titleOff : " ",
-		height: "80%"
+		height : "80%"
 	};
 	var _switch = Ti.UI.createSwitch(args);
 
@@ -95,13 +97,23 @@ function createFilterView(filter, allChecked) {
 }
 
 function closeWindow(e) {
+	displayMessageOnceDoneButtonIsClicked();
+	$.getView().close();
+}
+
+function displayMessageOnceDoneButtonIsClicked() {
+	// default message
+	var messageToDisplay = "The app content has been reorganized for the ages selected.";
+	selectedFilters = filter.formatActiveFiltersIntoArray(Alloy.Collections.filter);
+	if (JSON.stringify(selectedFilters) == "[]") {
+		messageToDisplay = "Customize by age will remain disabled because no age filters were selected.";
+	}
+
 	Titanium.UI.createAlertDialog({
 		title : '',
-		message : "The app content has been reorganized for the ages selected",
+		message : messageToDisplay,
 		ok : 'Got it!'
 	}).show();
-	
-	$.getView().close();
 }
 
 function addFilters(allChecked) {
